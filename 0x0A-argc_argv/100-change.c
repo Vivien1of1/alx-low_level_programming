74283103b3d6a5d0aa1a2d8f8411308ffaf420b3
vivien1of1 <viviennejeff@gmail.com>
2023-05-12 09:31:27 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,12 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
  * coinConverter - Helper function that does all the mathematics
  * @i: Passed in variable from main for calculations
  * Return: The number of coins needed minimum for the passed in variable
  */
+
 int coinConverter(int i)
 {
 	int count = 0;
